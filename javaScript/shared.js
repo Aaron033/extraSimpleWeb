a2bee0edfcc9cefed0868eee3f4ad5a386f41057
Aaron <aaron.botello.h@gmail.com>
2018-12-14 20:40:22 -0600

    javascript code of the .backdrop tag is finally working

---
+++ javaScript/shared.js
@@ -0,0 +1,15 @@
+var backdrop = document.querySelector('.backdrop');
+var modal= document.querySelector('.modal'); 
+
+//we are going to pick a button that is inside the .plan tag
+var selectPlanButtons = document.querySelectorAll('.prices button'); 
+
+
+for(var i = 0; i < selectPlanButtons.length; i++){
+    selectPlanButtons[i].addEventListener('click', function(){
+        modal.style.display= 'block';
+        backdrop.style.display='block'; 
+        
+    });
+    
+}
