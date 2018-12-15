187bb3e4e120f5df7359363c3a8bdd5f5ae3ea9b
Aaron <aaron.botello.h@gmail.com>
2018-12-15 17:58:06 -0600

    mobileNav javascript code created

--- javaScript/shared.js
+++ javaScript/shared.js
@@ -1,23 +1,32 @@
 var backdrop = document.querySelector('.backdrop');
-var modal= document.querySelector('.modal'); 
+var modal = document.querySelector('.modal');
 
 //we are going to pick a button that is inside the .plan tag
-var selectPlanButtons = document.querySelectorAll('.prices button'); 
+var selectPlanButtons = document.querySelectorAll('.prices button');
 
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
-for(var i = 0; i < selectPlanButtons.length; i++){
-    selectPlanButtons[i].addEventListener('click', function(){
-        modal.style.display= 'block';
-        backdrop.style.display='block'; 
-        
+for (var i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener('click', function () {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+
     });
-    
+
 }
 
-backdrop.addEventListener("click", closeModal);
+backdrop.addEventListener("click", function(){
+    mobileNav.style.display = "none"; 
+    this.closeModal();
+});
 modalNoButton.addEventListener("click", closeModal);
 
-function closeModal(){
-    backdrop.style.display ="none"; 
-    modal.style.display = "none"; 
+function closeModal() {
+    backdrop.style.display = "none";
+    modal.style.display = "none";
 }
+
+toggleButton.addEventListener('click', function () {
+    mobileNav.style.display = "block";
+})
